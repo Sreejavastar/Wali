c810e0faab3a5ae4c2591669362452856c011dc7
Harsha Gupta <guptaharsha.harsha@gmail.com>
2023-08-07 03:16:36 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
-import LoginPage from './components/LoginPage';
-
-// import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import NewPage from "./components/NewPage";
 
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/login" component={LoginPage} />
-    //     {/* <Route path="/main" component={MainPage} />
-    //     <Route path="/option1" component={Option1Page} />
-    //     <Route path="/option2" component={Option2Page} />
-    //     <Route path="/option3" component={Option3Page} /> */}
-    //   </  Routes>
-    // </Router>
-    <div>
-    <LoginPage/>
-   
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/newpage" element={<NewPage />} />
+      </Routes>
+    </Router>
   );
-    
 }
 
 export default App;
